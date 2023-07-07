995867253ddfcd0c17378329d7e63ef1dce9d384
Sergio Antozzi <santozzi@gmail.com>
2023-07-07 13:48:13 -0300

    poo

--- catalogo_peliculas.pyw
+++ catalogo_peliculas.pyw
@@ -3,7 +3,9 @@ from client.gui_app import *
 def main():
     root=tk.Tk()
     app = Frame(root=root)
+    
     barra_menu(root)
+    app.campos_peliculas()
     root.mainloop()
 if __name__ == '__main__':
     main()
--- client/gui_app.py
+++ client/gui_app.py
@@ -4,11 +4,18 @@ def barra_menu(root):
     barra_menu = tk.Menu(root)
     root.config(menu=barra_menu,width=300, height =300)
     menu_inicio = tk.Menu(barra_menu, tearoff=0)
+    menu_consultas = tk.Menu(barra_menu, tearoff=0) 
     barra_menu.add_cascade(label="inicio", menu=menu_inicio)
+    #opciones de inicio
     menu_inicio.add_command(label="Crear Registro en DB")
     menu_inicio.add_command(label="Eliminargistro en DB")
     menu_inicio.add_command(label="Salir", command= root.destroy)
-
+        
+    barra_menu.add_cascade(label="consultas", menu=menu_consultas)
+    #opciones de Consultas
+    menu_consultas.add_command(label="Crear Registro en DB")
+    menu_consultas.add_command(label="Eliminargistro en DB")
+    menu_consultas.add_command(label="Salir", command= root.destroy)
 
 
 class Frame(tk.Frame):
@@ -18,3 +25,7 @@ class Frame(tk.Frame):
         self.pack()
         self.config(width=500, height=350)
         self.config(bg="red")
+    
+    def campos_peliculas(self):
+        self.etiqueta_nombre = tk.Label(self,text="nombre: ")
+        self.etiqueta_nombre.grid(row=0,column=0)
